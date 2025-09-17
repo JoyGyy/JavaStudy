c17d15825295fa7ca56b5898f990c0744ad6439a
JoyGyy <k18372271132@outlook.com>
2025-09-17 16:08:21 +0800

    Update Test_7.java

--- demo_915/src/Test_7.java
+++ demo_915/src/Test_7.java
@@ -8,23 +8,21 @@ public class Test_7 {
         System.out.println(ming.getName());
         System.out.println(hong.getName());
     }
+}
 
-    class Person{
-        private String name;
+class Person{
+    private String name;
 
-        public void setName(String name){
-            this.name = name;
-        }
+    public void setName(String name){
+        this.name = name;
+    }
 
-        public void setName(String name1,String name2){
-            this.name = name1 + name2;
-        }
+    public void setName(String name1,String name2){
+        this.name = name1 + name2;
+    }
 
-        public String getName(){
-            return this.name;
-        }
-
+    public String getName(){
+        return this.name;
     }
 
 }
-
