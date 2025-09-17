8f207c9213265867d428a4a8be6aa38dfae694f8
JoyGyy <k18372271132@outlook.com>
2025-09-17 15:57:15 +0800

    小练习

---
+++ demo_915/src/Test_7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Test_7 {
+    public static void main(String[] args){
+        Person ming = new Person();
+        Person hong = new Person();
+        ming.setName("XiaoMing");
+        hong.setName("Xiao","Hong");
+        System.out.println(ming.getName());
+        System.out.println(hong.getName());
+    }
+}
+
+class Person{
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setName(String name1,String name2){
+        this.name = name1 + name2;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    
+}
